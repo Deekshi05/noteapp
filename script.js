f9c1acce710a8f76b8aebe61dbc42b95e924c041
Deekshi05 <163875958+Deekshi05@users.noreply.github.com>
2024-09-02 23:33:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let container=document.querySelector(".note-container");
 let opt=document.querySelector("button");
-let note=document.querySelector(".input-box");
+let show=()=>{
+    container.innerHTML=localStorage.getItem("notes");
+}
+show();
+let storageupdata=()=>{
+    localStorage.setItem("notes",container.innerHTML);
+}
 opt.addEventListener("click",()=>{
     let para=document.createElement("p");
     let img=document.createElement("img");
@@ -9,9 +15,29 @@ opt.addEventListener("click",()=>{
     img.className="delete";
     img.src="delete.png";
     container.appendChild(para).appendChild(img);
+    storageupdata();
 });
 container.addEventListener("click",(e)=>{
-   if(e.target.tagName=="IMG"){
+   if(e.target.tagName==="IMG"){
     e.target.parentElement.remove();
+    storageupdata();
+   }
+   else if(e.target.tagName==="P"){
+    let blocks=document.querySelectorAll(".input-box");
+    blocks.forEach(nt=>{
+        nt.onkeyup=()=>{
+            storageupdata();
+        }
+    });
    }
 })
+document.addEventListener("keydown",event=>{
+    if(event.key==="Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
+    }
+})
+
+
+
+
